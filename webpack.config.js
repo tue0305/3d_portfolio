040a55fed5bbfca97a55aeb899fe0991d7b56ad2
tue0305 <minhtuetql@gmail.com>
2025-03-10 22:49:58 +0700

    Deploy to GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/3d_portfolio/', // Update this line
+        publicPath: '/', // Update this line
     },
     module: {
         rules: [
